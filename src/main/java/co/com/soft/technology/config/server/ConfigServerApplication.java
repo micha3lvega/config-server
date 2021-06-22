a55776d752bf89081df1daa8f23f8a193b14d78e
Michael Vega <ec3572h@comcel.com.co>
2021-06-22 14:44:44 -0500

    Configuración para conexión al eureka server

--- src/main/java/co/com/soft/technology/config/server/ConfigServerApplication.java
+++ src/main/java/co/com/soft/technology/config/server/ConfigServerApplication.java
@@ -3,7 +3,9 @@ package co.com.soft.technology.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
